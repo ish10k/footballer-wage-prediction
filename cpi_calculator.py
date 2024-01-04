d222ff2c5655a664b4f13ab2439791b80942ef73
ish10k <111194744+ish10k@users.noreply.github.com>
2024-01-04 12:08:54 +0000

    update

---
+++ cpi_calculator.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+datasets_fp = "datasets/"
+# Import valuations dataset from csv
+_df = pd.read_csv(datasets_fp+"player_valuations.csv")
+
+def _clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    ...
+
+
+def calculate():
+    ...
